40abd51519bf571ac8e5efe04d856c96e023b90c
Jack Hardy <jackhardy1@gmail.com>
2016-06-08 20:04:37 +0100

    flag files as in use or not

--- lib/rails_cleaner.rb
+++ lib/rails_cleaner.rb
@@ -4,4 +4,33 @@ module RailsCleaner
   def self.hello
     puts "Hello world"
   end
+
+  def self.init(to)
+    files = Dir.glob("**/*")
+    array = []
+    files.each do |file|
+    if file.match(/.(scss|coffee|rb)/)
+    array << file
+  end
+  File.open (to), "w" do |f|
+    array.each do |line|
+      f.write line + "\n"
+      end
+    end
+  end
+
+  def self.sort(from,to)
+    new_array = []
+      File.foreach(from) do |line|
+      file = line.strip.gsub("'", "")
+        if File.ctime(file)==File.birthtime(file)
+          new_array << file
+        end
+      File.open to, "w" do |f|
+        new_array.each do |line|
+          f.write line + "\n"
+        end
+      end
+    end
+  end
 end
